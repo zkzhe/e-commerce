d3acba78a87db2f2f0b18859ee9dbc8bf1c64274
zkzhe <1zkzhe1146103@gmail.com>
2022-06-13 14:42:53 +0800

    fix(provider): 配合 cloudfare 導向 https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (App::environment('production')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
